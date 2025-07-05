0a698ccae3d2a43f7d824fa8daf6f11b663d35a8
gnsnghm <41932520+gnsnghm@users.noreply.github.com>
2025-07-05 14:26:34 +0900

    - Change date format at user page - delete palceholder.svg

--- frontend/app/me/page.tsx
+++ frontend/app/me/page.tsx
@@ -8,7 +8,18 @@ export default async function Me() {
     <main className="p-4 max-w-md mx-auto">
       <h1 className="text-xl font-bold mb-4">ユーザ情報</h1>
       <p>メール: {me.email}</p>
-      <p>登録日: {new Date(me.created_at).toLocaleDateString()}</p>
+      <p>
+        登録日:{" "}
+        {new Date(me.created_at).toLocaleString("ja-JP", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+          hour12: false,
+        })}
+      </p>
     </main>
   );
 }
